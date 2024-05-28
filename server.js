b1e8470975ad52079b7acca4e5a213e079a50970
Kenedy Maina <kennynet66@gmail.com>
2024-05-28 15:58:39 +0300

    refactored server code to start the json server

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-// JSON Server module
-const jsonServer = require("json-server");
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
-
-// Make sure to use the default middleware
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
 server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
-});
 
-// Export the Server API
-module.exports = server;
+server.listen(port);
